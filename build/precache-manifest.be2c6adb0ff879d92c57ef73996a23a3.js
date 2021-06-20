c380a1949feb94679645ba80dbcae54e009f7d52
sunny960 <sunnypardhan80@gmail.com>
2021-06-20 17:39:49 +0530

    build

---
+++ build/precache-manifest.be2c6adb0ff879d92c57ef73996a23a3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea10066ce52e10164a1f8a6ae172314d",
+    "url": "/demo-react-application/index.html"
+  },
+  {
+    "revision": "ee2e4c942fe0c3db59b9",
+    "url": "/demo-react-application/static/css/2.ebcfd34c.chunk.css"
+  },
+  {
+    "revision": "0b21a24a571337fc0ed2",
+    "url": "/demo-react-application/static/css/main.309ad80b.chunk.css"
+  },
+  {
+    "revision": "ee2e4c942fe0c3db59b9",
+    "url": "/demo-react-application/static/js/2.521491b3.chunk.js"
+  },
+  {
+    "revision": "5eceee41610a93fb8bfc4d1c690c67fc",
+    "url": "/demo-react-application/static/js/2.521491b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b21a24a571337fc0ed2",
+    "url": "/demo-react-application/static/js/main.e6ebc345.chunk.js"
+  },
+  {
+    "revision": "b8111d8347a5ec537053",
+    "url": "/demo-react-application/static/js/runtime-main.61a5edfb.js"
+  },
+  {
+    "revision": "ce45a9a9664ff731a806a1c8e52d86b3",
+    "url": "/demo-react-application/static/media/SFProText-Regular.ce45a9a9.ttf"
+  }
+]);
