e565bcf8c65543b5b972838837ef9bd1177ab444
sunny960 <sunnypardhan80@gmail.com>
2021-07-04 10:21:45 +0530

    add personalized workedout section

--- src/modules/session/sessionComponent.js
+++ src/modules/session/sessionComponent.js
@@ -4,12 +4,14 @@ import ReadyToJoinComponent from "../common/readyToJoinComponent";
 import Footer from "../footer";
 import GetOneOnLiveTraining from "./components/getOneOnLiveTraining";
 import StepView from "./components/stepView";
+import PersonalizedWorkout from "./components/personalizedWorkout";
 
 const SessionComponent = () => {
     return (<>
         <HeaderComponent hideHeaderMenu={true}/>
         <GetOneOnLiveTraining/>
         <StepView/>
+        <PersonalizedWorkout/>
         <ReadyToJoinComponent/>
         <Footer/>
 
