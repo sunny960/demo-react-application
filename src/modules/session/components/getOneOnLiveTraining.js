1181c178820f219432ab691cea71b8234f50ab4e
sunny960 <sunnypardhan80@gmail.com>
2021-07-04 21:48:53 +0530

    resolve some issue

--- src/modules/session/components/getOneOnLiveTraining.js
+++ src/modules/session/components/getOneOnLiveTraining.js
@@ -3,20 +3,13 @@ import styled from "styled-components";
 import HeaderMenuRow from "../../header/headerMenuRow";
 import Utility from "../../../utils";
 
-const Row = styled.div`
-  display: flex;
-  flex-direction: row;
-`
 const Column = styled.div`
   display: flex;
   flex-direction: column;
 `
 
-const ContentContainer = styled(Row)`
-  //align-items: center;
-  justify-content: center;
-  padding-top: 102px;
-  gap: 38px;
+const ContentContainer = styled(Column)`
+  position: relative;
 `
 const Container = styled(Column)`
   width: 100%;
@@ -25,38 +18,15 @@ const Container = styled(Column)`
 `
 
 const Icon = styled.img`
-  width: 525px;
-  height: 848px;
-`
-const LeftBtnImage = styled.img`
-  width: 328px;
-  height: 94px;
-`
-const RightBtnImage = styled.img`
-  width: 328px;
-  height: 94px;
-`
-const Title = styled.span`
-  font-family: Josefin Sans;
-  font-style: normal;
-  font-weight: bold;
-  font-size: 82px;
-  line-height: 110%;
-  /* or 90px */
-
-  display: flex;
-  align-items: center;
-  letter-spacing: -0.05em;
-  text-transform: uppercase;
-  color: #174C6E;
-  max-width: 585px;
-  padding: 0 0 31px 0;
-`
-const ImageBtnContainer = styled(Row)`
-  gap: 15px;
-  padding-bottom: 69px;
+  width: 100%;
+  height: 870px;
+  margin-top: 102px;
 `
 const PayNowButton = styled.button`
+  position: absolute;
+  left: 45%;
+  bottom: 20%;
+
   background: #FF5C15;
   border-radius: 100px;
   width: 228px;
@@ -80,17 +50,8 @@ const GetOneOnLiveTraining = () => {
     return (<Container>
         <HeaderMenuRow backgroundColor={'transparent'} showBtnAppyForTrainner={true}/>
         <ContentContainer>
-            <Column>
-                <Icon src={'/images/get_onlive_phone_image.png'}/>
-            </Column>
-            <Column>
-                <Title>{'Get 1 on Live Training on video call'}</Title>
-                <ImageBtnContainer>
-                    <LeftBtnImage src={'/images/get_onlive_training_first_btn_image.png'}/>
-                    <RightBtnImage src={'/images/get_onlive_training_2nd_btn_image.png'}/>
-                </ImageBtnContainer>
-                <PayNowButton onClick={() => Utility.showUnderDevelopment()}>{'Pay Now'}</PayNowButton>
-            </Column>
+            <Icon src={'/images/session_first.png'}/>
+            <PayNowButton onClick={() => Utility.showUnderDevelopment()}>{'Pay Now'}</PayNowButton>
         </ContentContainer>
 
     </Container>)
